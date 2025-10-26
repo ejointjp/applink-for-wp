23fb49cf3e65314330791cd232e5ac573f10ca16
Takashi Fujisaki <fujisaki@yuiami.jp>
2025-10-26 21:29:41 +0900

    プラグインヘッダーを修正

--- lito-applink.php
+++ lito-applink.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name:       LitoBlocks Applink
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       iPhone / iPad / Macアプリや音楽トラック、Apple Booksなどの紹介リンクを簡単に作成できます。
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
