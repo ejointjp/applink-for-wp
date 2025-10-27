83d6e804ce03d39400ad89e0f35cef07b4adf279
Takashi Fujisaki <fujisaki@yuiami.jp>
2025-10-27 22:46:09 +0900

    Refactor Applink component to use destructured props, update CSS class names, and adjust image border radius for improved styling.

--- src/components/Applink.jsx
+++ src/components/Applink.jsx
@@ -1,54 +1,48 @@
 import { HiPlay } from 'react-icons/hi';
 import { StoreIcon } from './StoreIcon';
 
-const Applink = (props) => {
+const Applink = ({ app }) => {
 	return (
-		<div className={`alfwp alfwp-${props.app.type}`}>
-			<div className='alfwp-link'>
+		<div className={`alfwp alfwp-${app.type}`}>
+			<a
+				className='alfwp-figure'
+				href={app.url}
+				target='_blank'
+				rel='noopener nofollow noreferrer'
+			>
+				<img className='alfwp-img' src={app.iconUrl} alt={app.title} />
+			</a>
+			<div className='alfwp-content'>
 				<a
-					className='alfwp-figure'
-					href={props.app.url}
+					className='alfwp-title'
+					href={app.url}
 					target='_blank'
 					rel='noopener nofollow noreferrer'
 				>
-					<img
-						className='alfwp-img'
-						src={props.app.iconUrl}
-						alt={props.app.title}
-					/>
+					{app.title}
 				</a>
-				<div className='alfwp-content'>
-					<a
-						className='alfwp-title'
-						href={props.app.url}
-						target='_blank'
-						rel='noopener nofollow noreferrer'
-					>
-						{props.app.title}
-					</a>
-					<div className='alfwp-artist'>{props.app.artist}</div>
+				<div className='alfwp-artist'>{app.artist}</div>
 
-					<div className='alfwp-btns'>
-						{props.app.previewUrl && (
-							<a
-								className='alfwp-audition alfwp-btn'
-								href={props.app.previewUrl}
-								target='_blank'
-								rel='noopener nofollow noreferrer'
-							>
-								<HiPlay />
-								<span className='alfwp-btn-label'>試聴</span>
-							</a>
-						)}
+				<div className='alfwp-btns'>
+					{app.previewUrl && (
 						<a
-							className='alfwp-store alfwp-btn'
-							href={props.app.url}
+							className='alfwp-audition alfwp-btn'
+							href={app.previewUrl}
 							target='_blank'
 							rel='noopener nofollow noreferrer'
 						>
-							<StoreIcon type={props.app.type} />
+							<HiPlay />
+							<span className='alfwp-btn-label'>試聴</span>
 						</a>
-					</div>
+					)}
+					<a
+						className='alfwp-store alfwp-btn'
+						href={app.url}
+						target='_blank'
+						rel='noopener nofollow noreferrer'
+					>
+						<StoreIcon type={app.type} />
+					</a>
 				</div>
 			</div>
 		</div>
